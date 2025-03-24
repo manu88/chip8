010179d470722ae89d900d673638ece1c07b97d4
manuel deneu <manuel.deneu@gmail.com>
2025-03-24 12:20:43 +0100

    Initial Commit

---
+++ chip8EMU/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chip8EMU
+//
+//  Created by Manuel Deneu on 24/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
