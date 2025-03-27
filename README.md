e17fd2f547ab18077885ecf3a17155d7740475fa
manuel deneu <manuel.deneu@gmail.com>
2025-03-27 09:52:05 +0100

    add args to emulator

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Chip8 emulator
 
+
+##some links
 https://en.wikipedia.org/wiki/CHIP-8
 http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 https://johnearnest.github.io/Octo/docs/SuperChip.html
--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,58 @@
 #include "SDLPeripherals.hpp"
 #include <iostream>
 
+bool checkFlag(int argc, const char *argv[], const char *flag) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], flag) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool getFlagValue(int argc, const char *argv[], const char *flag,
+                  std::string &val) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], flag) == 0) {
+            if (i + 1 < argc) {
+                val = std::string(argv[i + 1]);
+                return true;
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+void printUsage() {
+    printf("usage: inputfile [-h] [-v] \n");
+    printf("-h: this help\n");
+    printf("-v: verbose\n");
+}
+
 int main(int argc, const char *argv[]) {
+
+    if (checkFlag(argc, argv, "-h")) {
+        printUsage();
+        return 0;
+    }
+    if (argc < 2 || argv[1][0] == '-') {
+        printf("Error: no input file\n");
+        printUsage();
+        return 0;
+    }
+    bool verbose = false;
+    const std::string inputFile = argv[1];
+    if (checkFlag(argc, argv, "-v")) {
+        verbose = true;
+    }
 
     SDLPeripherals p;
     p.init();
     Rom rom;
-    rom.loadFile(
-        "/Users/manueldeneu/Documents/dev/chip8EMU/roms/helloworld.rom");
+    rom.loadFile(inputFile);
 
-    rom.dump();
-    Chip8::CPU emu({.logs = true});
+    Chip8::CPU emu({.logs = verbose});
     emu.init(&rom, &p);
     emu.run();
     return 0;
